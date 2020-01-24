bf6a2e9e1463d15d37def03ef0a0baad94132a2d
Nick Thompson <thompson.3155@buckeyemail.osu.edu>
2020-01-24 10:45:45 -0500

    Clean up format so it looks neat for teammates. Will plow through a lot more classes per commit now. This was just to get my bearings.

--- laravel/tests/Unit/AdminAddDataSetTest.php
+++ laravel/tests/Unit/AdminAddDataSetTest.php
@@ -11,18 +11,17 @@ use App\Milestone;
 
 class AdminAddDataSetTest extends TestCase {
     
-	/**
+    /**
      * A test of an admin controller method which adds a data set to a data table
-	 * and checks to ensure the data is there. Data is then deleted after the check.
+     * and checks to ensure the data is there. Data is then deleted after the check.
      *
      * @return void
      */
-	public function testAdminAddDataSetDataTableHasData()
-    {
-		$AC = new AdminController();
-		$AC->getUpdateMilestone('add', 'true', null, 'FakeAbbreviation', 'FakeFullName', 'FakeDetail');
-		$this->assertDatabaseHas('milestone', ['detail' => 'FakeDetail']);
-		$fakeData = Milestone::where('detail', 'FakeDetail')->first();
-		$fakeData->delete();
+    public function testAdminAddDataSetDataTableHasData() {
+        $AC = new AdminController();
+        $AC->getUpdateMilestone('add', 'true', null, 'FakeAbbreviation', 'FakeFullName', 'FakeDetail');
+        $this->assertDatabaseHas('milestone', ['detail' => 'FakeDetail']);
+        $fakeData = Milestone::where('detail', 'FakeDetail')->first();
+        $fakeData->delete();
     }	
 }
