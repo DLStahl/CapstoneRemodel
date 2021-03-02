d6a3ef9c3f42c3075c34fa7db84bd29c0c23743a
mmferguson <mmf98@live.com>
2021-03-01 20:11:24 -0500

    Updated AutoAssignment to account for anest prefs

--- laravel/app/AutoAssignment.php
+++ laravel/app/AutoAssignment.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Log;
 
 class AutoAssignment extends Model
 {
-    public static function assignment($date)
-    {
+    public static function assignment($date) {
         Log::info("auto assignment");
         if (Option::where('date', $date)->where('isValid', "1")->doesntExist()) {
             return;
@@ -25,25 +24,27 @@ class AutoAssignment extends Model
         
         //get array of resident ids
         $residents = Resident::pluck('id')->toArray();
+        $anestsAssigned = array();
         //handle 1st preference assignments with ticketsAdded = 0
-        $remainderFromFirstPref = self::assignResidentsForPref($residents, 1, 0, $date);
+        $arrayFromFirstPref = self::assignResidentsForPref($residents, 1, 0, $date, $anestsAssigned);
         //handle 2nd preference assignments with ticketsAdded = 1
-        $remainderFromSecondPref = self::assignResidentsForPref($remainderFromFirstPref, 2, 1, $date);
-        //handle 3rd preference assignments with ticketsAdded = 2
-        $remainderFromThirdPref = self::assignResidentsForPref($remainderFromSecondPref, 3, 2, $date);
-        //handle residents that are not assigned with ticketsAdded =3
-        foreach($remainderFromThirdPref as $unassignedResident){
-            if(Option::where('date', $date)
+        $arrayFromSecondPref = self::assignResidentsForPref($arrayFromFirstPref[0], 2, 2, $date, $arrayFromFirstPref[1]);
+        //handle 3rd preference assignments with ticketsAdded = 4
+        $arrayFromThirdPref = self::assignResidentsForPref($arrayFromSecondPref[0], 3, 4, $date, $arrayFromSecondPref[1]);
+        //handle residents that are not assigned with ticketsAdded = 6
+        foreach($arrayFromThirdPref[0] as $unassignedResident){
+            //if the resident made at least one preference then increase tickets
+            if (Option::where('date', $date)
                 ->where('resident', $unassignedResident)
                 ->count() > 0){
-                    self::updateProbability($unassignedResident, 3);
+                    self::updateProbability($unassignedResident, 6);
             }
         }
     }
 
     // Initialize total tickets if resident is not in Probability table
         // residents = array of resident objects
-    private static function initalizeResidentProbabilities($residents){
+    private static function initalizeResidentProbabilities($residents) {
         foreach($residents as $resident){
             if (Probability::where('resident', $resident['id'])->doesntExist()) {
                 Probability::insert([
@@ -56,12 +57,14 @@ class AutoAssignment extends Model
         }
     }
 
-    // Assigns residents for given preference number and returns array of residents not assigned 
+    // Assigns residents for given preference number
+    //returns Array of arrays [array of residents not assigned, array of assigned anests]
         // residents = array of resident ids
         // prefNum = Preference Number corresponding to option in option table
         // ticketsToAdd = number of tickets to give resident after assignment
         // date = date of schedules that are being assigned
-    private static function assignResidentsForPref($residents, $prefNum, $ticketsToAdd, $date){
+        // anestsAssigned = array that holds anesthesiologists already assigned where key=anest_id and value = number of assignments
+    private static function assignResidentsForPref($residents, $prefNum, $ticketsToAdd, $date, $anestsAssigned) {
         $remainder = array(); // remaining unassigned residents
         $wantedSchedules = array(); //schedule ids for schedules wanted by +1 residents
 
@@ -84,8 +87,13 @@ class AutoAssignment extends Model
                 ->where('option', $prefNum)
                 ->where('isValid', "1")
                 ->count() == 1){
-                    self::addAssignment($schedulePref, $resident, $date);
-                    self::updateProbability($resident, $ticketsToAdd); 
+                    $anestPref = Option::where('date', $date)
+                                ->where('schedule', $schedulePref)
+                                ->where('option', $prefNum)
+                                ->where('isValid', "1")
+                                ->where('resident', $resident)
+                                ->value('anesthesiologist_id');
+                    $anestsAssigned = self::handleAssignment($schedulePref, $resident, $date, $ticketsToAdd, $anestPref, $anestsAssigned); 
             // add schedule pref if it is not already in wantedSchedules array  
             } else if(!in_array($schedulePref, $wantedSchedules)){
                 array_push($wantedSchedules, $schedulePref);
@@ -136,8 +144,13 @@ class AutoAssignment extends Model
                 // residents not on rotation are added to remainder array
                 $remainder = array_merge($remainder, $notOnRotation);
                 // assign resident to schedule
-                self::addAssignment($wantedSchedule, $onRotation[0]['resident'], $date);
-                self::updateProbability($onRotation[0]['resident'], $ticketsToAdd);
+                $anestPref = Option::where('date', $date)
+                                ->where('schedule', $wantedSchedule)
+                                ->where('option', $prefNum)
+                                ->where('isValid', "1")
+                                ->where('resident', $onRotation[0]['resident'])
+                                ->value('anesthesiologist_id');
+                $anestsAssigned = self::handleAssignment($wantedSchedule, $onRotation[0]['resident'], $date, $ticketsToAdd, $anestPref, $anestsAssigned);
             }
             // Case 2: multiple residents on same rotation 
             else if(count($onRotation) > 1){
@@ -157,8 +170,13 @@ class AutoAssignment extends Model
                 // residents not on rotation are added to remainder array
                 $remainder = array_merge($remainder, $notOnRotation);
                 // assign winning resident to schedule
-                self::addAssignment($wantedSchedule, $winnerResident, $date);
-                self::updateProbability($winnerResident, $ticketsToAdd);
+                $anestPref = Option::where('date', $date)
+                                ->where('schedule', $wantedSchedule)
+                                ->where('option', $prefNum)
+                                ->where('isValid', "1")
+                                ->where('resident', $winnerResident)
+                                ->value('anesthesiologist_id');
+                $anestsAssigned = self::handleAssignment($wantedSchedule, $winnerResident, $date, $ticketsToAdd, $anestPref, $anestsAssigned);
             }
             // Case 3: No one is on same rotation as schedule
             else {
@@ -176,19 +194,69 @@ class AutoAssignment extends Model
                     }
                 }
                 // assign winning resident to schedule
-                self::addAssignment($wantedSchedule, $winnerResident, $date);
-                self::updateProbability($winnerResident, $ticketsToAdd);
+                $anestPref = Option::where('date', $date)
+                                ->where('schedule', $wantedSchedule)
+                                ->where('option', $prefNum)
+                                ->where('isValid', "1")
+                                ->where('resident', $winnerResident)
+                                ->value('anesthesiologist_id');
+                $anestsAssigned = self::handleAssignment($wantedSchedule, $winnerResident, $date, $ticketsToAdd, $anestPref, $anestsAssigned);
             }
         }
-        return $remainder;
+        return [$remainder,$anestsAssigned];;
+    }
+
+    // handles a resident's assignment checking if anest can be assigned and updating tickets 
+        // schedule = id for schedule 
+        // resident = id for resident
+        // date = date for schedule in YYYY-MM-DD format
+        // ticketsToAdd = initial number of tickets that will be added to resident's total
+        // anestPref = id for anesthesiologist
+        // anestsAssigned = array of assigned anests where key=anest_id and value=# of assignments
+    private static function handleAssignment($schedule, $resident, $date, $ticketsToAdd, $anestPref, $anestsAssigned) {
+        // if resident has an anesthesiologist pref - check if anest can be assigned
+        if(!is_null($anestPref)){
+            // get room
+            $room = ScheduleData::where('date', $date)
+                        ->where('id', $schedule)
+                        ->value('room');
+            $room = strval($room);
+            //if anest_id isn't a key in array -> anest hasn't been assigned yet
+            if(!array_key_exists($anestPref, $anestsAssigned)){
+                // CCCT and UH rooms can have anest assigned twice
+                //for leasing or temp rooms, default to CCCT instead of UH 
+                if($room != "" && strpos($room,"CCCT") !== false){;
+                    $anestsAssigned[$anestPref] = "CCCT";
+                } else if($room != "" && strpos($room,"UH") !== false) {
+                     $anestsAssigned[$anestPref] = "UH";
+                } else{
+                    // any other room type- anests can only be assigned once
+                    $anestsAssigned[$anestPref] = "Max Assignment";
+                }
+            // anest has already been assigned once
+            }else{
+                if($anestsAssigned[$anestPref] == "CCCT" && $room != "" && strpos($room,"CCCT") !== false){
+                    $anestsAssigned[$anestPref] = "Max Assignment";
+                }else if($anestsAssigned[$anestPref] == "UH" && $room != "" && strpos($room,"UH") !== false  && strpos($room,"CCCT") === false){
+                    $anestsAssigned[$anestPref] = "Max Assignment";
+                }else{
+                    // anest pref is not granted, give resident an extra ticket
+                    $anestPref = null;
+                    $ticketsToAdd++;
+                };
+            }
+        }
+        self::addAssignment($schedule, $resident, $date, $anestPref);
+        self::updateProbability($resident, $ticketsToAdd);
+        return $anestsAssigned;
     }
 
     // assign resident to schedule and update schedule to be taken
         // schedule = id for schedule 
         // resident = id for resident
         // date = date for schedule in YYYY-MM-DD format
-    private static function addAssignment($schedule, $resident, $date)
-    {
+        // anestId = id for anesthesiologist
+    private static function addAssignment($schedule, $resident, $date, $anestId) {
         // resident's other options/preferences made are now invalid
         Option::where('resident', $resident)->where('date', $date)->update([
             'isValid' => "0"
@@ -205,15 +273,19 @@ class AutoAssignment extends Model
         $option = Option::where('resident', $resident)->where('date', $date)->where('schedule', $schedule)->value('id');
 
         Assignment::insert([
-            'date' => $date, 'resident' => $resident, 'attending' => $attending, 'schedule' => $schedule, 'option' => $option
+            'date' => $date, 
+            'resident' => $resident, 
+            'attending' => $attending, 
+            'schedule' => $schedule, 
+            'option' => $option,
+            'anesthesiologist_id' => $anestId
         ]);
     }
 
     // updates resident's total value in Probability table
         // resident = id for resident
         // ticketsToAdd = number of tickets that will be added to resident's total
-    private static function updateProbability($resident, $ticketsToAdd)
-    {
+    private static function updateProbability($resident, $ticketsToAdd) {
         $total = Probability::where('resident', $resident)->value('total') + $ticketsToAdd;
         Probability::where('resident', $resident)->update([
             'total' => $total
