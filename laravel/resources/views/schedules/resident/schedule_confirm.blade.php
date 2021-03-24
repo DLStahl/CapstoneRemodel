dbe66dbd74e8baff3f75bab2ed4cc8a67b569a7f
mmferguson <mmf98@live.com>
2021-03-24 15:53:02 -0400

    update schedule confirm blade to use for each

--- laravel/resources/views/schedules/resident/schedule_confirm.blade.php
+++ laravel/resources/views/schedules/resident/schedule_confirm.blade.php
@@ -13,54 +13,52 @@
 			<h3>Your Current Selections</h3>
 		</div>
 	</div>
-	@for($i = 0; $i < 3; $i++)
+	@foreach($choiceTypes as $choiceType)
 		<div class="row">
-			<!-- Previous Choice -->
 			@if(!is_null($previousChoices[0]))
 				<div class="col">
-					<h4>{{ $choiceTypes[$i] }} Choice: </h4>
-					@if(is_null($previousChoices[$i]))
+					<h4>{{ $choiceType }} Choice: </h4>
+					@if(is_null($previousChoices[$loop->iteration - 1]))
 						<p>None</p>
 					@else
-						<p>Date: {{ $previousChoices[$i]['schedule'][0]['date'] }}</p>
-						<p>Room: {{ $previousChoices[$i]['schedule'][0]['room'] }}</p>
-						<p>Case Procedures:<br> {!! nl2br($previousChoices[$i]['case_procedure']) !!}</p>
-						<p>Milestone:<br> {{ $previousChoices[$i]['milestone'][0]['category'] }} - {{$previousChoices[$i]['milestone'][0]['detail'] }}</p>
-						<p>Objective:<br> {{ $previousChoices[$i]['objective'] }}</p>
-						@if ($previousChoices[$i]['anesthesiologist_pref'] != '[]')
-							<p>Preferred Anesthesiologist: <br> {{$previousChoices[$i]['anesthesiologist_pref'][0]['first_name']}} {{$previousChoices[$i]['anesthesiologist_pref'][0]['last_name']}}</p>	
+						<p>Date: {{ $previousChoices[$loop->iteration - 1]['schedule'][0]['date'] }}</p>
+						<p>Room: {{ $previousChoices[$loop->iteration - 1]['schedule'][0]['room'] }}</p>
+						<p>Case Procedures:<br> {!! nl2br($previousChoices[$loop->iteration - 1]['case_procedure']) !!}</p>
+						<p>Milestone:<br> {{ $previousChoices[$loop->iteration - 1]['milestone'][0]['category'] }} - {{$previousChoices[$loop->iteration - 1]['milestone'][0]['detail'] }}</p>
+						<p>Objective:<br> {{ $previousChoices[$loop->iteration - 1]['objective'] }}</p>
+						@if ($previousChoices[$loop->iteration - 1]['anesthesiologist_pref'] != '[]')
+							<p>Preferred Anesthesiologist: <br> {{$previousChoices[$loop->iteration - 1]['anesthesiologist_pref'][0]['first_name']}} {{$previousChoices[$loop->iteration - 1]['anesthesiologist_pref'][0]['last_name']}}</p>	
 						@else
 							<p>Preferred Anesthesiologist: <br> No Preference </p>
 						@endif
 					@endif
 				</div>
 			@endif
-			<!-- Current Choice -->
 			<div class="col">
-				<h4>{{ $choiceTypes[$i]  }} Choice: </h4>
-				@if(is_null($currentChoices[$i]))
+				<h4>{{ $choiceType }} Choice: </h4>
+				@if(is_null($currentChoices[$loop->iteration - 1]))
 					<p>None</p>
 				@else
-					<p>Date: {{ $currentChoices[$i]['schedule'][0]['date'] }}</p>
-					<p>Room: {{ $currentChoices[$i]['schedule'][0]['room'] }}</p>
-					<p>Case Procedures:<br> {!! nl2br($currentChoices[$i]['case_procedure']) !!}</p>
-					<p>Milestone:<br> {{ $currentChoices[$i]['milestone'][0]['category'] }} - {{$currentChoices[$i]['milestone'][0]['detail'] }}</p>
-					<p>Objective:<br> {{ $currentChoices[$i]['objective'] }}</p>
-					@if ($currentChoices[$i]['anesthesiologist_pref'] != '[]') 
-						<p>Preferred Anesthesiologist:<br> {{$currentChoices[$i]['anesthesiologist_pref'][0]['first_name'] }} {{ $currentChoices[$i]['anesthesiologist_pref'][0]['last_name']}}</p>
-						<input type="hidden" name="pref_anest{{$i+1}}" value="{{ $currentChoices[$i]['anesthesiologist_pref'][0]['id'] }}">
+					<p>Date: {{ $currentChoices[$loop->iteration - 1]['schedule'][0]['date'] }}</p>
+					<p>Room: {{ $currentChoices[$loop->iteration - 1]['schedule'][0]['room'] }}</p>
+					<p>Case Procedures:<br> {!! nl2br($currentChoices[$loop->iteration - 1]['case_procedure']) !!}</p>
+					<p>Milestone:<br> {{ $currentChoices[$loop->iteration - 1]['milestone'][0]['category'] }} - {{$currentChoices[$loop->iteration - 1]['milestone'][0]['detail'] }}</p>
+					<p>Objective:<br> {{ $currentChoices[$loop->iteration - 1]['objective'] }}</p>
+					@if ($currentChoices[$loop->iteration - 1]['anesthesiologist_pref'] != '[]') 
+						<p>Preferred Anesthesiologist:<br> {{$currentChoices[$loop->iteration - 1]['anesthesiologist_pref'][0]['first_name'] }} {{ $currentChoices[$loop->iteration - 1]['anesthesiologist_pref'][0]['last_name']}}</p>
+						<input type="hidden" name="pref_anest{{$loop->iteration}}" value="{{ $currentChoices[$loop->iteration - 1]['anesthesiologist_pref'][0]['id'] }}">
 					@else
 						<p> Preferred Anesthesiologist:<br> No Preference </p>
-						<input type="hidden" name="pref_anest{{$i+1}}" value=0>
+						<input type="hidden" name="pref_anest{{$loop->iteration}}" value=0>
 					@endif
-					<input type="hidden" name="option{{$i+1}}" value="{{$i+1}}">
-					<input type="hidden" name="milestones{{$i+1}}" value="{{ $currentChoices[$i]['milestone'][0]['id'] }}">
-					<input type="hidden" name="objectives{{$i+1}}" value="{{ $currentChoices[$i]['objective'] }}">
+					<input type="hidden" name="option{{$loop->iteration}}" value="{{$loop->iteration}}">
+					<input type="hidden" name="milestones{{$loop->iteration}}" value="{{ $currentChoices[$loop->iteration - 1]['milestone'][0]['id'] }}">
+					<input type="hidden" name="objectives{{$loop->iteration}}" value="{{ $currentChoices[$loop->iteration - 1]['objective'] }}">
 					
 				@endif
 			</div>
 		</div>
-	@endfor
+	@endforeach
 	<input type="submit" value="Confirm" class='btn btn-md btn-success'>
 </form>
 @endsection
