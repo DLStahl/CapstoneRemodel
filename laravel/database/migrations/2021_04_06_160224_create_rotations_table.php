4886eb4a0938ba6131a341728e78c4fb41b41453
Spencer Knabe <knabe.13@buckeyemail.osu.edu>
2021-04-06 16:32:34 -0400

    added 2 migrations

---
+++ laravel/database/migrations/2021_04_06_160224_create_rotations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRotationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rotations', function (Blueprint $table) {
+            $table->String('Name');
+            $table->bigIncrements('ID');
+            $table->Integer('Level');
+            $table->Integer('Service');
+            $table->String('Site');
+            $table->date('Start');
+            $table->date('End');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rotations');
+    }
+}
