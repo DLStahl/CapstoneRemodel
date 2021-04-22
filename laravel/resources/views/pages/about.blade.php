62e98732a6857977fc77d3d8cdb36b88a6918823
Cal Jacobson <cj81499@users.noreply.github.com>
2021-04-21 21:51:08 -0400

    cleanup about.blade.php

--- laravel/resources/views/pages/about.blade.php
+++ laravel/resources/views/pages/about.blade.php
@@ -6,62 +6,50 @@
             <h2>Personal Information</h2>
             <table class="table table-striped table-bordered" id="personal_info_table">
                 <tr>
-                    <td align="left">Name</td>
-                    <td aligh="left">{{ $data['name'] }}</td>
+                    <td>Name</td>
+                    <td>{{ $data['name'] }}</td>
                 </tr>
                 <tr>
-                    <td align="left">Email</td>
-                    <td align="left">{{ $data['email'] }}</td>
+                    <td>Email</td>
+                    <td>{{ $data['email'] }}</td>
                 </tr>
                 <tr>
-                    <td align="left">Role</td>
-                    @if (count($data['roles']) > 0)
-                        <td align="left">
-                            @foreach ($data['roles'] as $role)
-                                <p>{{ $role }}</p>
-                            @endforeach
-                        </td>
-                    @else
-                        <td align="left">None</td>
-                    @endif
-                </tr>
-                <tr>
-                    <td align="left">
-                        @php
-                            if (date('l', strtotime('today')) == 'Friday') {
-                                echo date('l', strtotime('+3 day')), ' ', date('F', strtotime('+3 day')), ' ', date('j', strtotime('+3 day'));
-                            } elseif (date('l', strtotime('today')) == 'Saturday') {
-                                echo date('l', strtotime('+2 day')), ' ', date('F', strtotime('+2 day')), ' ', date('j', strtotime('+2 day'));
-                            } else {
-                                echo date('l', strtotime('+1 day')), ' ', date('F', strtotime('+1 day')), ' ', date('j', strtotime('+1 day'));
-                            }
-                        @endphp
+                    <td>Role</td>
+                    <td>
+                        {{ count($data['roles']) > 0 ? join(', ', $data['roles']) : 'No roles' }}
                     </td>
-                    <td align="left">
+                </tr>
+                <tr>
+                    <td>
+                        @if (date('l', strtotime('today')) == 'Friday')
+                            {{ date('l F j', strtotime('+3 day')) }}
+                        @elseif (date('l', strtotime('today')) == 'Saturday')
+                            {{ date('l F j', strtotime('+2 day')) }}
+                        @else
+                            {{ date('l F j', strtotime('+1 day')) }} @endif
+                    </td>
+                    <td>
                         @if (!is_null($data['firstday']))
                             {!! nl2br($data['firstday']) !!}
                         @endif
                     </td>
                 </tr>
                 <tr>
-                    <td align="left">
-                        @php
-                            if (date('l', strtotime('today')) == 'Thursday') {
-                                echo date('l', strtotime('+4 day')), ' ', date('F', strtotime('+4 day')), ' ', date('j', strtotime('+4 day'));
-                            } elseif (date('l', strtotime('today')) == 'Friday') {
-                                echo date('l', strtotime('+4 day')), ' ', date('F', strtotime('+4 day')), ' ', date('j', strtotime('+4 day'));
-                            } elseif (date('l', strtotime('today')) == 'Saturday') {
-                                echo date('l', strtotime('+3 day')), ' ', date('F', strtotime('+3 day')), ' ', date('j', strtotime('+3 day'));
-                            } else {
-                                echo date('l', strtotime('+2 day')), ' ', date('F', strtotime('+2 day')), ' ', date('j', strtotime('+2 day'));
-                            }
-                        @endphp
+                    <td>
+                        @if (date('l', strtotime('today')) == 'Thursday')
+                            {{ date('l F j', strtotime('+4 day')) }}
+                        @elseif (date('l', strtotime('today')) == 'Friday')
+                            {{ date('l F j', strtotime('+4 day')) }}
+                        @elseif (date('l', strtotime('today')) == 'Saturday')
+                            {{ date('l F j', strtotime('+3 day')) }}
+                        @else
+                            {{ date('l F j', strtotime('+2 day')) }} @endif
                         @if (!is_null($data['secondday']['first']))
                             <br><button class="btn btn-md btn-success" onclick="milestones()">Edit <br>Milestones
                                 Objectives</button>
                         @endif
                     </td>
-                    <td align="left">
+                    <td>
                         @php
                             if (!is_null($data['secondday']['first'])) {
                                 $count = 0;
@@ -78,25 +66,22 @@
                     </td>
                 </tr>
                 <tr>
-                    <td align="left">
-                        @php
-                            if (date('l', strtotime('today')) == 'Wednesday') {
-                                echo date('l', strtotime('+5 day')), ' ', date('F', strtotime('+5 day')), ' ', date('j', strtotime('+5 day'));
-                            } elseif (date('l', strtotime('today')) == 'Thursday') {
-                                echo date('l', strtotime('+5 day')), ' ', date('F', strtotime('+5 day')), ' ', date('j', strtotime('+5 day'));
-                            } elseif (date('l', strtotime('today')) == 'Friday') {
-                                echo date('l', strtotime('+5 day')), ' ', date('F', strtotime('+5 day')), ' ', date('j', strtotime('+5 day'));
-                            } elseif (date('l', strtotime('today')) == 'Saturday') {
-                                echo date('l', strtotime('+4 day')), ' ', date('F', strtotime('+4 day')), ' ', date('j', strtotime('+4 day'));
-                            } else {
-                                echo date('l', strtotime('+3 day')), ' ', date('F', strtotime('+3 day')), ' ', date('j', strtotime('+3 day'));
-                            }
-                        @endphp
+                    <td>
+                        @if (date('l', strtotime('today')) == 'Wednesday')
+                            {{ date('l F j', strtotime('+5 day')) }}
+                        @elseif (date('l', strtotime('today')) == 'Thursday')
+                            {{ date('l F j', strtotime('+5 day')) }}
+                        @elseif (date('l', strtotime('today')) == 'Friday')
+                            {{ date('l F j', strtotime('+5 day')) }}
+                        @elseif (date('l', strtotime('today')) == 'Saturday')
+                            {{ date('l F j', strtotime('+4 day')) }}
+                        @else
+                            {{ date('l F j', strtotime('+3 day')) }} @endif
                         @if (!is_null($data['thirdday']['first']))
                             <br><button class="btn btn-md btn-success" onclick="milestones()">Edit milestones</button>
                         @endif
                     </td>
-                    <td align="left">
+                    <td>
                         @php
                             if (!is_null($data['thirdday']['first'])) {
                                 $count = 0;
@@ -113,17 +98,15 @@
                     </td>
                 </tr>
             </table>
-
-
             <script>
                 function milestones() {
                     var ids = "{{ $data['ids'] }}";
                     var current_url = window.location.href;
                     var url = current_url.substr(0, current_url.search('/resident/'));
                     url = url + "/resident/schedule/milestonesEdit/" + ids + "/";
-
                     window.location.href = url;
                 }
+
             </script>
         </div>
     </div>
@@ -131,5 +114,6 @@
         .btn {
             white-space: normal;
         }
+
     </style>
 @endsection
